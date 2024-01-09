3dd1d74229b5d3fcf05d0aef7bbbf7576cc3a3dd
Vinh Quang Tran <vinhquangtran@Vinh-MacBook-Pro.local>
2024-01-09 13:42:47 +0100

    first page

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = "https://ebkhmvscxyhwcgrbexuh.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVia2htdnNjeHlod2NncmJleHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MDI1MTUsImV4cCI6MjAyMDM3ODUxNX0.Q7_OQfJ9w7QKetChJisf5huGvmbh3E9wNdImDe7caKQ";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
