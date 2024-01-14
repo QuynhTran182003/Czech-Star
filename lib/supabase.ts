cb2bc4e74ad0319e99bdbfbf504e0f92a4025792
Vinh Quang Tran <vinhquangtran@Vinh-MacBook-Pro.local>
2024-01-15 00:07:30 +0100

    save minor in supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 
-const supabaseUrl = "https://ebkhmvscxyhwcgrbexuh.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVia2htdnNjeHlod2NncmJleHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MDI1MTUsImV4cCI6MjAyMDM3ODUxNX0.Q7_OQfJ9w7QKetChJisf5huGvmbh3E9wNdImDe7caKQ";
+export const supabaseUrl = "https://ebkhmvscxyhwcgrbexuh.supabase.co";
+export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVia2htdnNjeHlod2NncmJleHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ4MDI1MTUsImV4cCI6MjAyMDM3ODUxNX0.Q7_OQfJ9w7QKetChJisf5huGvmbh3E9wNdImDe7caKQ";
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
